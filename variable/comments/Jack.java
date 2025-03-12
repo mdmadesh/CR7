c4b506d4737656113139ac596e8cc70a68584051
mdmadesh <amongus1990jan1@gmail.com>
2025-03-12 14:15:07 +0530

    Program was successfully executed

---
+++ variable/comments/Jack.java
@@ -0,0 +1,8 @@
+public class Jack{
+    public static void main(String args[]){
+        int num = 30;
+        int num1 = 56;
+        System.out.println(num);
+        System.out.println(num1);
+    }
+    }
