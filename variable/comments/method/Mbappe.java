9368b41e67a3989f2c64983fa653bf871c00d3e7
mdmadesh <amongus1990jan1@gmail.com>
2025-03-12 14:33:28 +0530

    program was successfully completed

---
+++ variable/comments/method/Mbappe.java
@@ -0,0 +1,10 @@
+public class Mbappe{
+    public void add(int numOne , int numTwo){
+        int numThree = numOne + numTwo;
+        System.out.println(numThree);
+    }
+    public static void main(String args[]){
+        Mbappe obj = new Mbappe();
+        obj.add(84,56);
+    }
+}
