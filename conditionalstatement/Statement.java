fc5692f430bd978371c4bbf6a552a3b8e557a6ac
mdmadesh <amongus1990jan1@gmail.com>
2025-03-13 10:53:20 +0530

    The program was succesfully executed

---
+++ conditionalstatement/Statement.java
@@ -0,0 +1,11 @@
+public class Statement{
+    public static void main(String args[]){
+        if(false){
+            System.out.println(false);
+
+        }
+        if(true){
+            System.out.println(true);
+        }
+    }
+}
