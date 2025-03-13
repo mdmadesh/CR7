d6ea6098f6d691532afaf704bc85fab3d218c846
mdmadesh <amongus1990jan1@gmail.com>
2025-03-13 11:46:59 +0530

    The program was executed successfully

--- ifelse/Elseif.java
+++ ifelse/Elseif.java
@@ -1,9 +1,12 @@
-public class Own{
+public class Elseif{
     int a;
     public static void main(String[] args){
-        int a = 20;
+        int a = 10;
         if(a>0){
             System.out.println("a is positive");
+        }
+        else if(a==0){
+            System.out.println("a is neutral");
         }
         else{
             System.out.println("a is neagtive");
