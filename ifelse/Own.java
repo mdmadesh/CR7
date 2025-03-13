075827d3c91c4832d4c2b1c4bc3dbedd40d0709f
mdmadesh <amongus1990jan1@gmail.com>
2025-03-13 11:38:04 +0530

    The program is successfully executed

---
+++ ifelse/Own.java
@@ -0,0 +1,12 @@
+public class Own{
+    int a;
+    public static void main(String[] args){
+        int a = 20;
+        if(a>0){
+            System.out.println("a is positive");
+        }
+        else{
+            System.out.println("a is negative");
+        }
+    }
+}
