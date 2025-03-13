e9f536da9153dc4db45589e0c831df4efd37ad41
mdmadesh <amongus1990jan1@gmail.com>
2025-03-13 11:04:35 +0530

    The program was successfully executed

---
+++ ifelse/Statementone.java
@@ -0,0 +1,10 @@
+public class Statementone{
+    public static void main(String[] args){
+        if(true){
+            System.out.println(true);
+        }
+        else{
+            System.out.println(false);
+        }
+        }
+}
