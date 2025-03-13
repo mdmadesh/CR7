0404ba1f94a8b3fc093e16ec7adc062b02fdd7bb
mdmadesh <amongus1990jan1@gmail.com>
2025-03-13 14:28:16 +0530

    The program was executed successfully

---
+++ leapyear/Four.java
@@ -0,0 +1,12 @@
+public class Four{
+    int i;
+    public static void main(String[] args){
+        int i = 2020;
+        if(i%4==0){
+            System.out.println("Given year is leap year");
+        }
+        else{
+            System.out.println("Given yearbis non leap year");
+        }
+        }
+    }
