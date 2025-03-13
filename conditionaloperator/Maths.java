b5a93848c4d79f52f0758d138d707dd2777404e1
mdmadesh <amongus1990jan1@gmail.com>
2025-03-13 10:32:24 +0530

    The program was successfully executed

---
+++ conditionaloperator/Maths.java
@@ -0,0 +1,13 @@
+public class Maths{
+
+  
+    public static void main(String args[]){
+        System.out.println(10<20);
+        System.out.println(56>84);
+        System.out.println(2<=5);
+        System.out.println(986>=990);
+        System.out.println(11!=21);
+        System.out.println(56==56);
+
+    }
+}
