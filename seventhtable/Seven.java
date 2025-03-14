e3590be799963b6c6d5e6aad1dde8e7e9c0df72d
mdmadesh <amongus1990jan1@gmail.com>
2025-03-14 10:15:10 +0530

    The program was successfully executed

---
+++ seventhtable/Seven.java
@@ -0,0 +1,10 @@
+public class Seven{
+
+    public static void main(String[] args){
+        int i=1;
+        while(i<=10){
+            System.out.println(i + " x 7 = "+ i*7);
+            i++;
+        }
+    }
+    }
