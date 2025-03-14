dd2cdd37897bfa14f040099d1c3b919a5d965e85
mdmadesh <amongus1990jan1@gmail.com>
2025-03-14 10:02:50 +0530

    The program was successfully executed

---
+++ while/Fine.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Fine{
+    public static void main(String[] args){
+        int i=0;
+        while(i<10){
+            i=i+1;
+            System.out.println(i);
+
+    }
+}
+}
