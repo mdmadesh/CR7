4d426cf04daef796e8db86aa84048fcea70e8474
mdmadesh <amongus1990jan1@gmail.com>
2025-03-14 11:51:10 +0530

    The program was successfully executed

---
+++ split/Join.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Join{
+    public static void main(String[] args){
+        String name = "hello,world";
+        String[] names = name.split(",");
+        System.out.println(names[0]);
+    }
+}
