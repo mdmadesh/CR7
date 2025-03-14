dc3b86e3d8ab1e2a3b1e42cc1c45d99e26f26613
mdmadesh <amongus1990jan1@gmail.com>
2025-03-14 10:28:38 +0530

    The program was succesfully executed

---
+++ findnnumber/Nnn.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+
+public class Nnn{
+    public static void main(String[] args){
+        int i=0;
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int  n=obj.nextInt();
+        while(i<=n){
+            System.out.println(i);
+            i++;
+
+        }
+    }
+}
