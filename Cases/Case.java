437468153122f87f0353f6ea879cb96db1731e1e
mdmadesh <amongus1990jan1@gmail.com>
2025-03-14 12:05:42 +0530

    The program ws successfully executed

---
+++ Cases/Case.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Case{
+    public static void main(String[] args){
+        String name = "hello world";
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+
+    }
+}
