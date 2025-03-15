31ad98ac8a4cd12fcb744644780d482acad7d7d9
mdmadesh <amongus1990jan1@gmail.com>
2025-03-15 11:09:04 +0530

    The program was successfully executed

---
+++ dynamicaddition/Dye.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Dye{
+   public static void main(String[] args) {
+    Scanner scan=new Scanner(System.in);
+    System.out.println("enter the input:");
+    int a=scan.nextInt();
+    int b=scan.nextInt();
+    int c=a+b;
+    System.out.println("the sum of two numbers:"+c);
+   }
+    
+}
